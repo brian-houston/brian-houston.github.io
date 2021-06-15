000c27b839ad1890e8c34b5c2daef355cac5c5a7
Brian Houston <>
2021-06-15 10:29:07 -0400

    created project page layout

---
+++ projects/metadata.js
@@ -0,0 +1,11 @@
+const metadata = [
+    {
+        name: "NFL Scorigamis",
+        description: "A collection of graphs about unique scores that have occured in NFL games",
+        thumbnail: "thumbnails/scorigami.png",
+        address: "https://observablehq.com/@brian-houston/nfl-scorigami-tracker",
+        sortWeight: 100,
+    },
+]
+
+export default metadata;
