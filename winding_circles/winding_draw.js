e706be88f2adc27fafa1e2556fc28667e0605c3f
Brian Houston <>
2023-11-01 19:42:38 -0400

    remove Point class

--- winding_circles/winding_draw.js
+++ winding_circles/winding_draw.js
@@ -2,13 +2,6 @@ function clamp(n, min, max) {
   return Math.max(Math.min(n, max), min);
 }
 
-class Point {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-
 class Edge {
   constructor(p1, p2, height) {
     this.m = (p2.x - p1.x) / (p2.y - p1.y);
