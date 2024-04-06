45d9506abccd017ffad8beb1b9367d7e38de6f71
Brian Houston <>
2024-04-05 20:42:57 -0400

    remove old file, rename updated version

--- scorigamis/chart.js
+++ scorigamis/chart.js
@@ -1,100 +1,50 @@
-import "../libraries/d3.v7.min.js";
-import {Legend} from "../libraries/legend.js";
-import {load_nfl_data} from "../libraries/nfl_data.js";
+import "../utilities/d3.v7.min.js";
+import {Legend} from "../utilities/legend.js";
+import {loadNFLData} from "../utilities/nfl_data.js";
+import {makeScorigamiChart} from "../utilities/scorigami_chart.js" 
 
-let data = await load_nfl_data();
+let data = await loadNFLData();
 let dict = d3.group(data, d => d.strScore);
 data = [...dict.values()].map(d => d[0]);
 data = data.filter(d => !isNaN(d.scores[0]));
 
-let margins = {
-  top: 10,
-  bottom: 40,
-  left: 10,
-  right: 40,
-}
+const width = 1000;
+const seasonExtent = d3.extent(data.map(d => d.season));
+const colorScale = d3.scaleSequential(seasonExtent, 
+  d => d3.color(d3.interpolateSpectral(d)).darker(0.2));
+const [svg, updateChart] = makeScorigamiChart({width: width, data: data, colorScale: d => colorScale(d.season)});
 
-let width = 1000;
-let maxMin = d3.max(data.map(d => d.scores[0]));
-let maxMax = d3.max(data.map(d => d.scores[1]));
-let height = (maxMin+1) * (width-margins.left-margins.right) / (maxMax+1) + margins.top + margins.bottom;
-let paddingInner = 0.1;
+const colorLegend = Legend(colorScale,
+  {
+    title: 'Season',
+    tickFormat: "d",
+    width: 340
+  });
 
-let chartWidth = width - margins.left - margins.right;
-let chartHeight = height - margins.top - margins.bottom;
-
-const xScale = d3.scaleBand(d3.range(0, maxMax + 1), [margins.left, width - margins.right])
-  .paddingInner(paddingInner);
-
-const yScale = d3.scaleBand(d3.range(0, maxMin + 1), [height - margins.bottom, margins.top])
-  .paddingInner(paddingInner);
-
-let yearExtent = d3.extent(data.map(d => d.season));
-const colorScale = d3.scaleSequential(yearExtent, t => d3.color(d3.interpolateSpectral(t)).darker(0.2));
-
-const xAxis = d3.axisBottom(xScale)
-  .tickFormat(d => d % 5 == 0 ? d : '')
-  .tickSizeOuter(0);
-
-const yAxis = d3.axisRight(yScale)
-  .tickFormat(d => d % 5 == 0 ? d : '')
-  .tickSizeOuter(0);
-
-const svg = d3.create('svg')
-  .attr('width', width)
-  .attr('height', height);
-
-svg.append('g')
-  .selectAll('line')
-  .data(xScale.domain().filter(d => d % 5 == 0))
-  .join('line')
-  .attr('x1', d => xScale(d) + xScale.bandwidth()/2)
-  .attr('y1', height - margins.bottom)
-  .attr('x2', d => xScale(d) + xScale.bandwidth()/2)
-  .attr('y2', margins.top)
-  .attr('stroke', 'lightgray')
-  .attr('stroke-dasharray', '2,2');
-
-svg.append('g')
-  .selectAll('line')
-  .data(yScale.domain().filter(d => d % 5 == 0 && d != 0))
-  .join('line')
-  .attr('x1', width - margins.right)
-  .attr('y1', d => yScale(d) + yScale.bandwidth()/2)
-  .attr('x2', margins.left)
-  .attr('y2', d => yScale(d) + yScale.bandwidth()/2)
-  .attr('stroke', 'lightgray')
-  .attr('stroke-dasharray', '2,2');
-
-svg.append('g')
-  .attr('transform', `translate(0,${height - margins.bottom})`)
-  .call(xAxis)
+const titleElement = svg.select('#scori-title');
+svg.append(() => colorLegend.node())
+  .attr('x', titleElement.attr('x'))
+  .attr('y', parseInt(titleElement.attr('y')) + 30)
+  .attr('font-family', 'monospace')
+  .select('g')
   .attr('font-family', 'monospace');
 
-svg.append('g')
-  .attr('transform', `translate(${width - margins.right}, 0)`)
-  .call(yAxis)
-  .attr('font-family', 'monospace');
+updateChart(data);
 
-let containerWidth = 130;
-let containerHeight = 28;
-let containerYOffset = 10;
-svg.append('g')
+const containerWidth = 130;
+const containerHeight = 28;
+const containerYOffset = 10;
+const dateFormatter = d3.timeFormat('%m-%d-%Y')
+svg.select('#scori-squares')
   .selectAll('rect')
-  .data(data)
-  .join('rect')
-  .attr('width', xScale.bandwidth())
-  .attr('height', yScale.bandwidth())
-  .attr('x', d => xScale(d.scores[1]))
-  .attr('y', d => yScale(d.scores[0]))
-  .attr('fill', d => colorScale(d.season))
   .on('mouseover', function(e, d) {
-    d3.select(this).attr('stroke', 'black');
+    const thisElement = d3.select(this);
+    thisElement.attr('stroke', 'black');
     let container = svg.append('g')
       .attr('id', d.strScore)
 
-    let cx = xScale(d.scores[1]) + xScale.bandwidth()/2;
-    let cy = yScale(d.scores[0]) + yScale.bandwidth() + containerYOffset;
+    let cx = parseInt(this.getAttribute('x')) + parseInt(this.getAttribute('width'))/2;
+    let cy = parseInt(this.getAttribute('y')) + parseInt(this.getAttribute('height')) + containerYOffset;
 
     cx = Math.min(Math.max(cx, containerWidth/2 + 1), width - containerWidth/2 - 1);
 
@@ -107,7 +57,7 @@ svg.append('g')
       .attr('stroke', 'black')
 
     container.append('text')
-      .text(d.date)
+      .text(dateFormatter(d.date))
       .attr('x', cx)
       .attr('y', cy + 2)
       .attr('text-anchor', 'middle')
@@ -126,50 +76,5 @@ svg.append('g')
     d3.select(this).attr('stroke', null)
     svg.select(`#${d.strScore}`).remove();
   })
-
-let titleStartX = margins.left + 25;
-let titleStartY = margins.top + 65;
-svg.append('g')
-  .append('text')
-  .text('NFL Scorigamis')
-  .attr('x', titleStartX)
-  .attr('y', titleStartY)
-  .attr('font-size', 40)
-  .attr('font-family', 'monospace')
-
-svg.append('g')
-  .append('text')
-  .text('Winning Score')
-  .attr('x', margins.left + chartWidth * 0.5)
-  .attr('y', height - 4)
-  .attr('font-size', 15)
-  .attr('font-family', 'monospace')
-  .attr('text-anchor', 'middle')
-
-svg.append('g')
-  .append('text')
-  .text('Losing Score')
-  .attr('transform', 'rotate(90)')
-  .attr('x', margins.top + chartHeight * 0.5)
-  .attr('y', -width)
-  .attr('font-size', 15)
-  .attr('font-family', 'monospace')
-  .attr('text-anchor', 'middle')
-  .attr('dominant-baseline', 'hanging')
-
-const colorLegend = Legend(colorScale,
-  {
-    title: 'Season',
-    tickFormat: "d",
-    width: 340
-  });
-
-
-svg.append(() => colorLegend.node())
-  .attr('x', titleStartX)
-  .attr('y', titleStartY + 30)
-  .attr('font-family', 'monospace')
-  .select('g')
-    .attr('font-family', 'monospace')
 
 document.body.prepend(svg.node());
