dc5453ad320384946624f8d526ecb408fee8afdf
Brian Houston <>
2024-04-05 20:18:52 -0400

    fixed nfl distribution chart

--- nfl_dist/chart.js
+++ nfl_dist/chart.js
@@ -1,109 +1,13 @@
-import "../libraries/d3.v7.min.js";
-import {Legend} from "../libraries/legend.js"; 
-import {load_nfl_data} from "../libraries/nfl_data.js";
+
+import "../utilities/d3.v7.min.js";
+import {Legend} from "../utilities/legend.js";
+import {load_nfl_data} from "../utilities/nfl_data.js";
+import {makeScorigamiChart} from "../utilities/scorigami_chart.js" 
 
 let data = await load_nfl_data();
-let decadeExtent = d3.extent(data.map(d => d.season));
-
-let margins = {
-  top: 10,
-  bottom: 40,
-  left: 10,
-  right: 40,
-}
-
-let width = 1000;
-let maxMin = d3.max(data.map(d => d.scores[0]));
-let maxMax = d3.max(data.map(d => d.scores[1]));
-let height = (maxMin+1) * (width-margins.left-margins.right) / (maxMax+1) + margins.top + margins.bottom;
-let paddingInner = 0.1;
-
-let chartWidth = width - margins.left - margins.right;
-let chartHeight = height - margins.top - margins.bottom;
-
-const xScale = d3.scaleBand(d3.range(0, maxMax + 1), [margins.left, width - margins.right])
-  .paddingInner(paddingInner);
-
-const yScale = d3.scaleBand(d3.range(0, maxMin + 1), [height - margins.bottom, margins.top])
-  .paddingInner(paddingInner);
 
 const colorScale = d3.scaleSequentialLog([1, 30], t => d3.color(d3.interpolateGnBu(t)).darker(0.2));
-
-const xAxis = d3.axisBottom(xScale)
-  .tickFormat(d => d % 5 == 0 ? d : '')
-  .tickSizeOuter(0);
-
-const yAxis = d3.axisRight(yScale)
-  .tickFormat(d => d % 5 == 0 ? d : '')
-  .tickSizeOuter(0);
-
-const svg = d3.create('svg')
-  .attr('width', width)
-  .attr('height', height);
-
-svg.append('g')
-  .selectAll('line')
-  .data(xScale.domain().filter(d => d % 5 == 0))
-  .join('line')
-  .attr('x1', d => xScale(d) + xScale.bandwidth()/2)
-  .attr('y1', height - margins.bottom)
-  .attr('x2', d => xScale(d) + xScale.bandwidth()/2)
-  .attr('y2', margins.top)
-  .attr('stroke', 'lightgray')
-  .attr('stroke-dasharray', '2,2');
-
-svg.append('g')
-  .selectAll('line')
-  .data(yScale.domain().filter(d => d % 5 == 0 && d != 0))
-  .join('line')
-  .attr('x1', width - margins.right)
-  .attr('y1', d => yScale(d) + yScale.bandwidth()/2)
-  .attr('x2', margins.left)
-  .attr('y2', d => yScale(d) + yScale.bandwidth()/2)
-  .attr('stroke', 'lightgray')
-  .attr('stroke-dasharray', '2,2');
-
-svg.append('g')
-  .attr('transform', `translate(0,${height - margins.bottom})`)
-  .call(xAxis)
-  .attr('font-family', 'monospace');
-
-svg.append('g')
-  .attr('transform', `translate(${width - margins.right}, 0)`)
-  .call(yAxis)
-  .attr('font-family', 'monospace');
-
-
-let titleStartX = margins.left + 25;
-let titleStartY = margins.top + 65;
-svg.append('g')
-  .append('text')
-  .text('NFL Scorigamis')
-  .attr('id', 'title')
-  .attr('x', titleStartX)
-  .attr('y', titleStartY)
-  .attr('font-size', 40)
-  .attr('font-family', 'monospace')
-
-svg.append('g')
-  .append('text')
-  .text('Winning Score')
-  .attr('x', margins.left + chartWidth * 0.5)
-  .attr('y', height - 4)
-  .attr('font-size', 15)
-  .attr('font-family', 'monospace')
-  .attr('text-anchor', 'middle')
-
-svg.append('g')
-  .append('text')
-  .text('Losing Score')
-  .attr('transform', 'rotate(90)')
-  .attr('x', margins.top + chartHeight * 0.5)
-  .attr('y', -width)
-  .attr('font-size', 15)
-  .attr('font-family', 'monospace')
-  .attr('text-anchor', 'middle')
-  .attr('dominant-baseline', 'hanging')
+const [svg, updateChart] = makeScorigamiChart({data: data, colorScale: d => colorScale(d.count)});
 
 const colorLegend = Legend(colorScale,
   {
@@ -114,52 +18,23 @@ const colorLegend = Legend(colorScale,
     tickValues: [1, 5, 10, 20, 30]
   });
 
+const titleElement = svg.select('#scori-title');
 svg.append(() => colorLegend.node())
-  .attr('x', titleStartX)
-  .attr('y', titleStartY + 30)
+  .attr('x', titleElement.attr('x'))
+  .attr('y', parseInt(titleElement.attr('y')) + 30)
   .attr('font-family', 'monospace')
   .select('g')
     .attr('font-family', 'monospace')
 
 document.body.prepend(svg.node());
 
-const boxes = svg.append('g');
+let seasonExtent = d3.extent(data.map(d => d.season));
+let year = seasonExtent[0] - 10;
+setInterval(() => {
+  svg.select('#scori-title')
+    .text(`NFL Score Distribution ${year}-${year+9}`);
 
-function drawBoxes(data) {
-  boxes.selectAll('rect')
-    .data(data, d => d.strScore)
-    .join(
-      enter => enter.append('rect')
-        .attr('width', 0)
-        .attr('height', 0)
-        .attr('x', d => xScale.bandwidth()/2 + xScale(d.scores[1]))
-        .attr('y', d => yScale.bandwidth()/2 + yScale(d.scores[0])),
-      update => update,
-      exit => exit.transition()
-        .duration(200)
-        .attr('width', 0)
-        .attr('height', 0)
-        .attr('x', d => xScale.bandwidth()/2 + xScale(d.scores[1]))
-        .attr('y', d => yScale.bandwidth()/2 + yScale(d.scores[0]))
-        .remove(),
-    )
-    .transition()
-    .duration(200)
-    .attr('width', xScale.bandwidth())
-    .attr('height', yScale.bandwidth())
-    .attr('x', d => xScale(d.scores[1]))
-    .attr('y', d => yScale(d.scores[0]))
-    .attr('fill', d => colorScale(d.count))
-}
-
-let year = decadeExtent[0];
-function frame() {
-  if (year <= decadeExtent[1]) {
-    svg.select('#title')
-      .text(`NFL Scores - ${year}s`);
-  }
-
-  let anim_data = data.filter(d => d.decade == year);
+  let anim_data = data.filter(d => d.season >= year && d.season < year + 10);
   let anim_groups = d3.group(anim_data, d => d.strScore);
   let count_data = [...anim_groups.values()].map(d => {
     return {
@@ -168,13 +43,11 @@ function frame() {
       strScore: d[0].strScore
     }
   })
-  drawBoxes(count_data);
-  year+=10;
+  updateChart(count_data);
+  year++;
 
-  if (year > decadeExtent[1]) {
-    year = decadeExtent[0];
+  if (year > seasonExtent[1]) {
+    year = seasonExtent[0] - 10;
   }
-}
+}, 220)
 
-frame();
-setInterval(frame, 1000)
